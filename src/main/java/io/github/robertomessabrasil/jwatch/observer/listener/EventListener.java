8298e209c6827fc4616238623e9aae42ecbb2c28
Roberto Messa <wardog1@gmail.com>
2024-07-20 15:29:58 -0300

    fix: source updated to resolve sonarqube issues

--- src/main/java/io/github/robertomessabrasil/jwatch/observer/listener/EventListener.java
+++ src/main/java/io/github/robertomessabrasil/jwatch/observer/listener/EventListener.java
@@ -3,17 +3,17 @@ package io.github.robertomessabrasil.jwatch.observer.listener;
 import java.util.ArrayList;
 import java.util.List;
 
-abstract public class EventListener {
-    final private List<Class<? extends Event>> eventsOfInterest = new ArrayList<>();
+public abstract class EventListener {
+    private final List<Class<? extends Event>> eventsOfInterest = new ArrayList<>();
 
     public abstract boolean handleEvent(Event event);
 
-    final public EventListener addEvent(Class<? extends Event> event) {
+    public final EventListener addEvent(Class<? extends Event> event) {
         this.eventsOfInterest.add(event);
         return this;
     }
 
-    final public List<Class<? extends Event>> getEventsOfInterest() {
+    public final List<Class<? extends Event>> getEventsOfInterest() {
         return this.eventsOfInterest;
     }
 
